f5bb4af00073e77ef54186022ad710afe1c0531b
Marat <125448969+martini69lol@users.noreply.github.com>
2023-05-11 19:43:08 +0300

    Update individual.py

--- individual.py
+++ individual.py
@@ -1,6 +1,10 @@
-def split_tuple():
-    word = input('>>> ')
-    result = tuple(value for value in set(word) if word.count(value) < 2)
-    print(result)
-    
-split_tuple()
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    def split_tuple():
+        word = input('>>> ')
+        result = tuple(value for value in set(word) if word.count(value) < 2)
+        print(result)
+
+ split_tuple()
